bb23f82b5e403928b9ffccc121d6877b415bcab8
witmess <matakalanew@gmail.com>
2025-05-08 18:18:27 +0200

    Enhance Billing, Business, and CardDeal components with improved layout and styling; update styles for section images.

--- Bank_Modern_app/src/Components/Business.jsx
+++ Bank_Modern_app/src/Components/Business.jsx
@@ -4,8 +4,16 @@ import styles, {layout} from '../style';
 
 const FeatureCard = ({icon, title, content, index}) => (
   <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length - 1 ? 'mb-6' : 'mb-0'} feature-card`}>
-  <div>
+  <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
     <img src={icon} alt={icon} className='w-[50%] h-[50%] object-contain'/>
+  </div>
+  <div className='flex-1 flex flex-col ml-3'>
+    <h4 className='font-poppins font-semibold text-[18px] leading-[23px] text-white mb-1'>
+      {title}
+    </h4>
+    <p className='font-poppins font-normal text-[16px] leading-[24px] text-dimWhite mb-1'>
+      {content}
+    </p>
   </div>
 
   </div>
@@ -20,7 +28,7 @@ const Business = () => {
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}> with the right credit card, you can improve your financial life by building credit, earning 
       rewards and saving money. But with hundreds of credit cards on the market</p>
 
-      <Button styles="mt-10"/>
+      <Button styles="mt-10 rounded-[20px]"/>
     </div>
     <div className={`${layout.sectionImg}flex-col`}>
     {features.map((feature, index) => (<FeatureCard key={feature.id}{
